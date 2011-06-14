502fe5236296c2c3f69fa7cbcf2352c44da34361
Florian Diesch <diesch@localhost>
2011-06-14 21:39:30 +0200

    * classicmenu_indicator/cmindicator.py   - restructed code   - added menus from settings.menu   - added 'ClassicMenu Indicator' submenu

--- classicmenu_indicator/cmindicator.py
+++ classicmenu_indicator/cmindicator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8-*-
 #
-# classicmenu-indicator - an indicator showing the main menu from Gnome Classic
+# ClassicMenu Indicator - an indicator applet for Unity, that 
+#                         provides the main menu of Gnome2/Gnome Classic. 
 #
 # Copyright (C) 2011 Florian Diesch <devel@florian-diesch.de>
 #
@@ -27,6 +28,7 @@ import gobject
 import gtk
 import appindicator
 import re
+import textwrap
 import subprocess
 from optparse import OptionParser
 import xdg.IconTheme as xdgicon
@@ -37,101 +39,185 @@ APP_VERSION = "0.03"
 re_command = re.compile('%[UFuf]')
 cmd_terminal = 'gnome-terminal -e'
 
+include_nodisplay = False
 
-def on_menuitem_activate(menuitem, entry):
-    command = entry.get_exec()
-    if command:        
-        command=re_command.sub('', command)
-        if entry.get_launch_in_terminal():
-            command = '%s %s' % (cmd_terminal, command)
-        subprocess.Popen(command, shell=True)
+class ClassicMenuIndicator(object):
+    def __init__(self):
+        self.indicator = appindicator.Indicator("classicmenu-indicator",
+                                                "start-here",
+                                                appindicator.CATEGORY_SYSTEM_SERVICES)
+        self.indicator.set_status (appindicator.STATUS_ACTIVE)
+
+        self.menu = self.create_menu()
+        self.indicator.set_menu(self.menu)
+
+    def run(self):
+        try:
+            gtk.main()
+        except KeyboardInterrupt:
+            pass
 
 
-def create_menu_item(entry):
-    
-    icon = entry.get_icon()
-    name = entry.get_name()
 
-    menu_item = gtk.ImageMenuItem(name)
+    def create_menu_item(self, entry):    
+        icon = entry.get_icon()
+        name = entry.get_name()
 
-    if (icon):
         menu_item = gtk.ImageMenuItem(name)
-        img =  gtk.Image()
+
+        if (icon):
+            menu_item = gtk.ImageMenuItem(name)
+            img =  gtk.Image()
         
-        icon_path = xdgicon.getIconPath(icon)
+            icon_path = xdgicon.getIconPath(icon)
 
-        if icon_path:
-            img.set_from_file(icon_path)
+            if icon_path:
+                img.set_from_file(icon_path)
+            else:
+                img.set_from_icon_name(icon, 64)
+
+
+            menu_item.set_image(img)
+            menu_item.set_always_show_image(True)
         else:
-            img.set_from_icon_name(icon, 64)
-
-
-        menu_item.set_image(img)
-        menu_item.set_always_show_image(True)
-    else:
-       menu_item = gtk.MenuItem(name) 
+            menu_item = gtk.MenuItem(name) 
            
-    if entry.get_type() ==  gmenu.TYPE_ENTRY:
-        menu_item.connect('activate', on_menuitem_activate, entry)
+        if entry.get_type() ==  gmenu.TYPE_ENTRY:
+            menu_item.connect('activate', self.on_menuitem_activate, entry)
 
-    menu_item.show()    
-    return menu_item
+        menu_item.show()
+        return menu_item
 
 
-def process_entry(menu, entry):
-    menu.append(create_menu_item(entry))
+    def process_entry(self, menu, entry):
+        menu.append(self.create_menu_item(entry))
 
     
-def process_directory(menu, dir):
-    if dir:
-        for item in dir.get_contents():
-            type = item.get_type()
-            if type == gmenu.TYPE_ENTRY:
-                process_entry(menu, item)
-            elif type == gmenu.TYPE_DIRECTORY:
-                new_menu = gtk.Menu()
-                menu_item = create_menu_item(item)
-                menu_item.set_submenu(new_menu)
-                menu.append(menu_item)
-                menu_item.show()
-                process_directory(new_menu, item)
-            elif type == gmenu.TYPE_ALIAS:
-                aliased = item.get_item()
-                if aliased.get_type() == gmenu.TYPE_ENTRY:
-                    process_entry(menu, aliased)
-            elif type == gmenu.TYPE_SEPARATOR:
-                menu_item = gtk.SeparatorMenuItem()
-                menu.append(menu_item)
-                menu_item.show()
-            elif type in [ gmenu.TYPE_HEADER ]:
-                pass
-            else:
-                print >> sys.stderr, 'Unsupported item type: %s' % type
+    def process_directory(self, menu, dir):
+        if dir:
+            for item in dir.get_contents():
+                type = item.get_type()
+                if type == gmenu.TYPE_ENTRY:
+                    self.process_entry(menu, item)
+                elif type == gmenu.TYPE_DIRECTORY:
+                    new_menu = gtk.Menu()
+                    menu_item = self.create_menu_item(item)
+                    menu_item.set_submenu(new_menu)
+                    menu.append(menu_item)
+                    menu_item.show()
+                    self.process_directory(new_menu, item)
+                elif type == gmenu.TYPE_ALIAS:
+                    aliased = item.get_item()
+                    if aliased.get_type() == gmenu.TYPE_ENTRY:
+                        self.process_entry(menu, aliased)
+                elif type == gmenu.TYPE_SEPARATOR:
+                    menu_item = gtk.SeparatorMenuItem()
+                    menu.append(menu_item)
+                    menu_item.show()
+                elif type in [ gmenu.TYPE_HEADER ]:
+                    pass
+                else:
+                    print >> sys.stderr, 'Unsupported item type: %s' % type
+
+
+    def add_to_menu(self, menu, name):
+        tree = self.create_tree(name)
+        root = tree.get_root_directory()    
+        self.process_directory(menu, root)
+
+
+    def create_menu(self):
+        menu = gtk.Menu()
+        self.add_to_menu(menu, 'applications.menu')
+        menu_item = gtk.SeparatorMenuItem()
+        menu.append(menu_item)
+        self.add_to_menu(menu, 'settings.menu')
+
+        menu_item = gtk.SeparatorMenuItem()
+        menu.append(menu_item)
+
+        menu_item = gtk.MenuItem('%s'%APP_NAME)
+        menu.append(menu_item)
+
+        submenu = gtk.Menu()
+        menu_item.set_submenu(submenu)
+
+        menu_item = gtk.ImageMenuItem(gtk.STOCK_ABOUT)
+        menu_item.connect('activate', self.on_menuitem_about_activate)
+        submenu.append(menu_item)
+        
+        menu_item = gtk.SeparatorMenuItem()
+        submenu.append(menu_item)
+        menu_item = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+        menu_item.connect('activate', self.on_menuitem_quit_activate)
+        submenu.append(menu_item)
+
+        menu.show_all()
+        return menu;
+
+    def create_tree(self, name):
+        flags = gmenu.FLAGS_NONE
+        if include_nodisplay:
+            flags = flags | gmenu.FLAGS_INCLUDE_NODISPLAY
+        tree = gmenu.lookup_tree(name, flags)
+        tree.add_monitor(self.on_menu_changed, self.indicator)
+        return tree
 
 
 
-def on_menu_changed(tree, ind):
-    ind.set_menu(create_menu(tree))
+#####################
+## Signal-Behandlung
+#####################
 
-def create_menu(tree):
-    menu = gtk.Menu()
-    root = tree.get_root_directory()    
-    process_directory(menu, root)
-    return menu;
+    def on_menuitem_activate(self, menuitem, entry):
+        command = entry.get_exec()
+        if command:        
+            command=re_command.sub('', command)
+            if entry.get_launch_in_terminal():
+                command = '%s %s' % (cmd_terminal, command)
+            subprocess.Popen(command, shell=True)
 
-def create_tree(ind):
-    menu_file = 'applications.menu'
-    flags = gmenu.FLAGS_NONE
-    tree = gmenu.lookup_tree(menu_file, flags)
-    tree.add_monitor(on_menu_changed, ind)
-    return tree
 
-def create_indicator():
-    ind = appindicator.Indicator ("classicmenu-indicator",
-                                  "start-here",
-                                  appindicator.CATEGORY_SYSTEM_SERVICES)
-    ind.set_status (appindicator.STATUS_ACTIVE)
-    return ind;
+    def on_menu_changed(self, tree, ind):
+        ind.set_menu(create_menu(tree))
+
+    def on_menuitem_quit_activate(self, menuitem):
+        quit()
+
+    def on_menuitem_about_activate(self, menuitem):
+        dlg = gtk.AboutDialog()
+        dlg.set_name(APP_NAME)
+        dlg.set_version(APP_VERSION)
+        dlg.set_website('http://www.florian-diesch.de/software/classicmenu-indicator/')
+        dlg.set_authors(['Florian Diesch <devel@florian-diesch.de>'])
+        dlg.set_copyright('Copyright (c) 2011 Florian Diesch')
+        dlg.set_license(textwrap.dedent(
+            """
+            ClassicMenu Indicator - an indicator applet for Unity, 
+            that provides the main menu of Gnome2/Gnome Classic. 
+
+           
+            Copyright © 2011 Florian Diesch <devel@florian-diesch.de>
+           
+            Homepage: http://www.florian-diesch.de/software/classicmenu-indicator/
+           
+            This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+           
+            This program is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU General Public License for more details.
+           
+            You should have received a copy of the GNU General Public License
+            along with this program.  If not, see <http://www.gnu.org/licenses/>.
+            """))
+
+        dlg.run()
+        dlg.destroy()
+
 
 def parse_args():
     parser = OptionParser(version="%s %s"%(APP_NAME, APP_VERSION))
@@ -140,15 +226,9 @@ def parse_args():
 
 def main():
     parse_args()
-    ind = create_indicator()    
-    tree = create_tree(ind)
-    menu = create_menu(tree)
-    ind.set_menu(menu)
-    gtk.main()
-
+    indicator = ClassicMenuIndicator()
+    indicator.run()   
 
 if __name__ == '__main__':
-    try:
-      main()
-    except KeyboardInterrupt:
-      pass
+    main ()
+    
