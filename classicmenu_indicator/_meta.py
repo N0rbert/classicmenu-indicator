fd1585cf25632ee225703c58e1ab8e4737cbd7c4
Florian Diesch <diesch@localhost>
2015-07-18 02:57:10 +0200

    * added classicmenu_indicator/_meta.py (thanks to Jose Paternina for spotting this)

---
+++ classicmenu_indicator/_meta.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+# This file is generated. All changes will be lost
+
+VERSION="0.09-1"
+TITLE="ClassicMenu Indicator"
+NAME="classicmenu-indicator"
+AUTHOR_NAME="Florian Diesch"
+AUTHOR_EMAIL="devel@florian-diesch.de"
+WEB_URL="http://www.florian-diesch.de/software/classicmenu-indicator/"
+TIMESTAMP="2014-09-25T18:39:37"
