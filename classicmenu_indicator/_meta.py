dfa1ddb6bb85fe8d2f65b0a2e201da76a46d5dd0
Florian Diesch <diesch@esprimo.daheim>
2017-10-25 19:26:32 +0200

    Version 0.11

--- classicmenu_indicator/_meta.py
+++ classicmenu_indicator/_meta.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # This file is generated. All changes will be lost
 
-VERSION = "0.11pr1"
+VERSION = "0.11"
 TITLE = "ClassicMenu Indicator"
 NAME = "classicmenu-indicator"
 AUTHOR_NAME = "Florian Diesch"
 AUTHOR_EMAIL = "devel@florian-diesch.de"
 WEB_URL = "http://www.florian-diesch.de/software/classicmenu-indicator/"
-TIMESTAMP = "2017-10-13T17:36:26"
+TIMESTAMP = "2017-10-25T18:17:46"
